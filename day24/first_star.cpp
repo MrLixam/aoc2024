e010537a69b2e1ff2015c49c69d5a722f78b56b7
Liam VINCENT <lixam.vincent@gmail.com>
2024-12-02 04:56:25 +0100

    base structure

---
+++ day24/first_star.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+
+int main(void)
+{
+	std::ifstream file;
+	file.open("input.txt");
+}

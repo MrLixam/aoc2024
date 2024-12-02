977e06b6a14f8ba2b5badd3f655777ce26292287
Liam VINCENT <lixam.vincent@gmail.com>
2024-12-02 07:35:21 +0100

    day 2 done my god did the second star take me a while

--- day2/first_star.cpp
+++ day2/first_star.cpp
@@ -5,8 +5,69 @@
 #include <vector>
 #include <sstream>
 
+int abs(int first, int second)
+{
+	return(first > second ? first - second : second - first);
+}
+
 int main(void)
 {
 	std::ifstream file;
 	file.open("input.txt");
+
+	int valid = 0;
+	std::string current;
+	while(getline(file, current))
+	{
+		std::stringstream sstream(current);
+		std::cout << current << std::endl;
+		std::vector<int> list;
+		while (!sstream.eof())
+		{
+			int n;
+			sstream >> n;
+			std::cout << n << std::endl;
+			list.push_back(n);
+		}
+		bool down = false;
+		bool up = false;
+
+		std::vector<int>::iterator it;
+		for (it = list.begin(); it != list.end(); it++)
+		{
+			if (std::next(it) == list.end())
+				continue;
+			int next = *std::next(it);
+			if (next < *it)
+			{
+				down = true;
+				std::cout << "next smaller that current" << std::endl;
+				std::cout << next << " < " << *it << std::endl;
+			}
+			if (next > *it)
+			{
+				up = true;
+				std::cout << "next bigger that current" << std::endl;
+				std::cout << next << " > " << *it << std::endl;
+			}
+			if (next == *it)
+			{
+				std::cout << "equal numbers" << std::endl;
+				break;
+			}
+			if (up && down)
+			{
+				std::cout << "inverse directions" << std::endl;
+				break;
+			}
+			if (abs(*it, next) > 3)
+			{
+				std::cout << "gap too big" << std::endl;
+				break;
+			}
+		}
+		if (it == list.end())
+			valid++;
+	}
+	std::cout << valid;
 }
--- day2/second_star.cpp
+++ day2/second_star.cpp
@@ -5,8 +5,74 @@
 #include <vector>
 #include <sstream>
 
+int abs(int first, int second)
+{
+	return(first > second ? first - second : second - first);
+}
+
+bool is_safe(bool direction, int current, int next)
+{
+	if (current == next)
+		return (false);
+	bool current_direction = current < next ? 0 : 1;
+	if (current_direction != direction)
+		return (false);
+	
+	int difference = abs(current, next);
+	if (abs(current, next) > 3)
+		return (false);
+	return (true);
+}
+
+bool test_list(std::vector<int> list)
+{
+	bool direction = list[0] < list[1] ? 0 : 1;
+
+	std::vector<int>::iterator it;
+	for (it = list.begin(); std::next(it) != list.end(); it++)
+	{
+		if (!is_safe(direction, *it, *std::next(it)))
+		{
+			return (false);
+		}
+	}
+	return (true);
+}
+
 int main(void)
 {
 	std::ifstream file;
 	file.open("input.txt");
+
+	int valid = 0;
+	std::string current;
+	while(getline(file, current))
+	{
+		std::stringstream sstream(current);
+		std::vector<int> list;
+		while (!sstream.eof())
+		{
+			int n;
+			sstream >> n;
+			//std::cout << n << std::endl;
+			list.push_back(n);
+		}
+		if (test_list(list))
+		{
+			valid++;
+			continue;
+		}
+		for (int i = 0; i < list.size(); i++)
+		{
+			std::vector<int> copy;
+			std::copy(list.begin(), list.end(), std::back_inserter(copy));
+			copy.erase(copy.begin() + i);
+			if (test_list(copy))
+			{
+				valid++;
+				break;
+			}
+		}
+	}
+	std::cout << valid;
 }

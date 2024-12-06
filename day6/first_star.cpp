3c5466e9fecb3c706c177d9d7290fc1875c561ff
Liam VINCENT <lixam.vincent@gmail.com>
2024-12-06 07:57:41 +0100

    spent too long optimizing second star lmao

--- day6/first_star.cpp
+++ day6/first_star.cpp
@@ -9,4 +9,49 @@ int main(void)
 {
 	std::ifstream file;
 	file.open("input.txt");
+	std::vector<std::string> map;
+
+	int direction_x = -1, direction_y = 0;
+	int x = -1, y = -1;
+	std::string input;
+	while(getline(file, input))
+	{
+		map.push_back(input);
+		std::string::iterator it = find(input.begin(), input.end(), '^');
+		if (it != input.end())
+		{
+			x = map.size() - 1;
+			y = std::distance(input.begin(), it);
+		}
+	}
+	if (x == -1)
+		return 1;
+	long long iterations = 0;
+	while(true)
+	{
+		if (x + direction_x < 0 || x + direction_x >= map.size())
+			break;
+		if (y + direction_y < 0 || y + direction_y >= map[x].size())
+			break;
+		if (map[x + direction_x][y + direction_y] == '#')
+		{
+			int newX = direction_y;
+			int newY = -direction_x;
+
+			direction_x = newX;
+			direction_y = newY;
+		}
+		else
+		{
+			if (map[x + direction_x][y + direction_y] == '.')
+				iterations++;
+			map[x][y] = 'X';
+			map[x + direction_x][y + direction_y] = '^';
+			x = x+direction_x;
+			y = y+direction_y;
+		}
+		std::cout << x << " " << y << std::endl;
+	}
+	iterations++;
+	std::cout << iterations << std::endl;
 }
--- day6/second_star.cpp
+++ day6/second_star.cpp
@@ -4,9 +4,73 @@
 #include <algorithm>
 #include <vector>
 #include <sstream>
+#include <tuple>
+#include <chrono>
+
+bool try_iteration(std::vector<std::string>& map, int x, int y)
+{
+	int dx[] = {-1, 0, 1, 0};
+	int dy[] = {0, 1, 0, -1};
+	int direction = 0;
+	int nx, ny;
+	std::vector<std::tuple<int, int, int, int>> visited;
+
+	while(true)
+	{
+		std::tuple<int, int, int, int> current = std::make_tuple(x, y, dx[direction], dy[direction]);
+		if (find(visited.begin(), visited.end(), current) != visited.end())
+			return true;
+		visited.push_back(current);
+		while (true)
+		{
+			nx = x + dx[direction];
+			ny = y + dy[direction];
+
+			if (nx < 0 || nx >= 130 || ny < 0 || ny >= 130)
+				return false;
+			if (map[nx][ny] == '#')
+			{
+				direction = (direction + 1) % 4;
+				break;
+			}
+			x = nx;
+			y = ny;
+		}
+	}
+}
 
 int main(void)
 {
 	std::ifstream file;
 	file.open("input.txt");
+	std::vector<std::string> map;
+
+	int direction_x = -1, direction_y = 0;
+	int x = -1, y = -1;
+	std::string input;
+	while(getline(file, input))
+	{
+		map.push_back(input);
+		std::string::iterator it = find(input.begin(), input.end(), '^');
+		if (it != input.end())
+		{
+			x = map.size() - 1;
+			y = std::distance(input.begin(), it);
+		}
+	}
+	if (x == -1)
+		return 1;
+	long long possible = 0;
+	for (int i = 0; i < map.size(); ++i) {
+		for (int j = 0; j < map[i].size(); ++j)
+		{
+			if (map[i][j] != '.' || (i == x && j == y))
+				continue;
+			map[i][j] = '#';
+			if (try_iteration(map, x, y))
+				possible++;
+			map[i][j] = '.';
+		}
+	}
+	std::cout << possible << std::endl;
 }

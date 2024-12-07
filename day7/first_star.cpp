3a23e28772f3c7a177a884637753c6145033aede
Liam VINCENT <lixam.vincent@gmail.com>
2024-12-07 06:55:42 +0100

    day7 passed I liked this one

--- day7/first_star.cpp
+++ day7/first_star.cpp
@@ -5,8 +5,48 @@
 #include <vector>
 #include <sstream>
 
+bool obtainable(long long value, long long current, std::vector<long long>& numbers, int index)
+{
+	if (current > value)
+		return false;
+	if (index == numbers.size())
+		return (current == value);
+	
+	if (obtainable(value, current + numbers[index], numbers, index + 1))
+		return true;
+	if (current > 0 && obtainable(value, current * numbers[index], numbers, index + 1))
+		return true;
+	return false;
+}
+
 int main(void)
 {
 	std::ifstream file;
 	file.open("input.txt");
+
+	std::string current;
+	unsigned long long total = 0;
+	while (getline(file, current))
+	{
+		std::string value_str;
+		std::stringstream sstream(current);
+		getline(sstream, value_str, ':');
+		long long value = atoll(value_str.c_str());
+		std::vector<long long> numbers;
+		//std::cout << current << std::endl;
+		//std::cout << value << ": ";
+		while (true)
+		{
+			long long tmp = -1;
+			sstream >> tmp;
+			if (tmp == -1)
+				break;
+			// std::cout << tmp << " ";
+			numbers.push_back(tmp);
+		}
+		// std::cout << std::endl;
+		if (obtainable(value, 0, numbers, 0))
+			total += value;
+	}
+	std::cout << total << std::endl;
 }
--- day7/second_star.cpp
+++ day7/second_star.cpp
@@ -5,8 +5,51 @@
 #include <vector>
 #include <sstream>
 
+long long combine(long long first, long long second)
+{
+	return std::stoull(std::to_string(first) + std::to_string(second));
+}
+
+bool obtainable(long long value, long long current, std::vector<long long>& numbers, int index)
+{
+	if (current > value)
+		return false;
+	if (index == numbers.size())
+		return (current == value);
+	
+	if (obtainable(value, current + numbers[index], numbers, index + 1))
+		return true;
+	if (current > 0 && obtainable(value, current * numbers[index], numbers, index + 1))
+		return true;
+	if (obtainable(value, combine(current, numbers[index]), numbers, index + 1))
+		return true;
+	return false;
+}
+
 int main(void)
 {
 	std::ifstream file;
 	file.open("input.txt");
+
+	std::string current;
+	unsigned long long total = 0;
+	while (getline(file, current))
+	{
+		std::string value_str;
+		std::stringstream sstream(current);
+		getline(sstream, value_str, ':');
+		long long value = atoll(value_str.c_str());
+		std::vector<long long> numbers;
+		while (true)
+		{
+			long long tmp = -1;
+			sstream >> tmp;
+			if (tmp == -1)
+				break;
+			numbers.push_back(tmp);
+		}
+		if (obtainable(value, 0, numbers, 0))
+			total += value;
+	}
+	std::cout << total << std::endl;
 }

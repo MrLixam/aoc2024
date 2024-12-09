51322586e39093ac77c8499e3316aefacbad346c
Liam VINCENT <lixam.vincent@gmail.com>
2024-12-09 08:33:42 +0100

    day9 my god did I struggle on the second star

--- day9/first_star.cpp
+++ day9/first_star.cpp
@@ -5,8 +5,61 @@
 #include <vector>
 #include <sstream>
 
+void printVector(const std::vector<std::string>& vec)
+{
+	for (const auto& str : vec)
+		std::cout << str << std::endl;
+}
+
 int main(void)
 {
 	std::ifstream file;
 	file.open("input.txt");
+
+	std::string line;
+	std::vector<std::string> disk;
+	getline(file, line);
+	size_t id = 0;
+	for (size_t i = 0; i < line.size(); i++)
+	{
+		int times = line[i] - '0';
+		if (i % 2 == 0)
+		{
+			std::string number = std::to_string(id);
+			for (int j = 0; j < times; j++)
+			{
+				disk.push_back(number);
+			}
+			id++;
+		}
+		else
+		{
+			for (int j = 0; j < times; j++)
+			{
+				disk.push_back(".");
+			}
+		}
+	}
+	std::string list = ".";
+	while (true)
+	{
+		std::vector<std::string>::iterator number;
+		for (auto it = disk.rbegin(); it != disk.rend(); it++)
+		{
+			if (*it != ".")
+			{
+				number = it.base();
+				number--;
+				break;
+			}
+		}
+		break;
+	}
+	unsigned long long value = 0;
+	for (size_t i = 0; disk[i] != "."; i++)
+	{
+		int tmp = atol(disk[i].c_str());
+		value += i * tmp;
+	}
+	std::cout << value << std::endl;
 }
--- day9/second_star.cpp
+++ day9/second_star.cpp
@@ -5,8 +5,98 @@
 #include <vector>
 #include <sstream>
 
+struct my_file {
+	std::string id;
+	size_t start;
+	size_t len;
+};
+
+size_t find_space(size_t start, size_t length, std::vector<std::string>& disk)
+{
+	size_t free_length = 0;
+	size_t valid_index = std::string::npos;
+	size_t current_index = 0;
+	for (size_t i = 0; i < start; i++)
+	{
+		if (disk[i] == ".")
+		{
+			if (free_length == 0)
+				current_index = i;
+			free_length++;
+			if (free_length >= length)
+			{
+				valid_index = current_index;
+				break;
+			}
+		}
+		else
+			free_length = 0;
+	}
+	return (valid_index);
+}
+
+void move_file(std::vector<std::string>& disk, my_file& file, size_t index)
+{
+	for (size_t i = 0; i < file.len; i++)
+	{
+		disk[index + i] = file.id;
+	}
+
+	for (size_t i = file.start; i < file.start + file.len; i++)
+	{
+		disk[i] = ".";
+	}
+	file.start = index;
+}
+
+void printVector(const std::vector<std::string>& vec)
+{
+	for (const auto& str : vec)
+		std::cout << str << " ";
+	std::cout << std::endl;
+}
+
 int main(void)
 {
-	std::ifstream file;
-	file.open("input.txt");
+	std::ifstream file("input.txt");
+	std::string line;
+	getline(file, line);
+
+	std::vector<std::string> disk;
+	std::vector<my_file> files;
+	size_t id = 0;
+	for (size_t i = 0; i < line.size(); i++)
+	{
+		int times = line[i] - '0';
+		if (i % 2 == 0)
+		{
+			std::string number = std::to_string(id);
+			size_t start = disk.size();
+			for (int j = 0; j < times; j++)
+				disk.push_back(number);
+			files.push_back({number, start, (size_t)times});
+			id++;
+		}
+		else
+			for (int j = 0; j < times; j++)
+				disk.push_back(".");
+	}
+	
+	for (std::vector<my_file>::reverse_iterator it = files.rbegin(); it != files.rend(); it++)
+	{
+		my_file file = *it;
+		size_t new_index = find_space(file.start, file.len, disk);
+		if (new_index != std::string::npos)
+			move_file(disk, *it, new_index);
+	}
+
+	unsigned long long value = 0;
+	for (size_t i = 0; i < disk.size(); i++)
+	{
+		if (disk[i] == ".")
+			continue;
+		int tmp = atol(disk[i].c_str());
+		value += i * tmp;
+	}
+	std::cout << value << std::endl;
 }
